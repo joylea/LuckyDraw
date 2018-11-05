959c4617b0b7f619e60d399fcfe78183e290146c
Li <qiao-li@mercer.com>
2018-11-05 11:39:05 +0800

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {MaterialModule} from './module/material.module';
 import { LuckyDrawComponent } from './lucky-draw/lucky-draw.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lucky-draw/lucky-draw.component.ts
+++ src/app/lucky-draw/lucky-draw.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Award} from '../model/award.model';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-lucky-draw',
   templateUrl: './lucky-draw.component.html',
-  styleUrls: ['./lucky-draw.component.css']
+  styleUrls: ['./lucky-draw.component.css'],
 })
 export class LuckyDrawComponent implements OnInit {
   drawFinished = false;
   participantList = [];
   awardsList: Award[] = [];
   currentAward: Award;
+  currentWinner = '';
   finishedAwards: Award[] = [];
   winners = [];
-  drawButtonText = '开始抽奖';
   isDrawInProgress = false;
   displayText = '';
+  closeResult: string;
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
     this.participantList = ['Gloria', 'Rita', 'Cecilia', 'Cary', 'Danny', 'Joe'];
     const secondPrize: Award = {
       allowDuplicate: false,
@@ -25,7 +27,7 @@ export class LuckyDrawComponent implements OnInit {
       name: '蛋糕',
       quantity: 3};
     const firstPrize: Award = {
-      allowDuplicate: true,
+      allowDuplicate: false,
       imageSrc: '../../assets/image/1.jpg',
       name: '手机',
       quantity: 1};
@@ -42,62 +44,72 @@ export class LuckyDrawComponent implements OnInit {
 
   switchAward() {
     if (this.awardsList.length > 0) {
-      this.currentAward = this.awardsList[0];
-      let hasAllAwardFinished = false;
+      if (!this.currentAward) {
+        this.currentAward = this.awardsList[0];
+      }
+      let hasAllAwardFinished = true;
+      let tmpAward: Award;
       for (let i = 0; i < this.awardsList.length; i++) {
-        // TODO: fix this logic issue
+        let isCurrentAwardFinished = false;
         for (const finishedItem of this.finishedAwards) {
-          console.log(finishedItem.name);
-          if (finishedItem !== this.awardsList[i]) {
-            hasAllAwardFinished = false;
+          if (this.awardsList[i] === finishedItem) {
+            isCurrentAwardFinished = true;
             break;
           }
         }
-        if (!hasAllAwardFinished) {
-          this.currentAward = this.awardsList[i];
+        if (isCurrentAwardFinished === false) {
+          tmpAward = this.awardsList[i];
+          hasAllAwardFinished = false;
           break;
-        } else {
-          this.drawFinished = true;
         }
       }
+      if (!hasAllAwardFinished) {
+        this.currentAward = tmpAward;
+      } else {
+        this.drawFinished = true;
+      }
+    } else {
+      this.drawFinished = true;
     }
   }
 
   // draw one award one time.
   startDraw() {
+    this.isDrawInProgress = !this.isDrawInProgress;
+  }
+  stopDraw() {
     if (this.isDrawInProgress) {
       const awardWinners = [];
       for (let i = 0; i < this.currentAward.quantity; i++) {
         awardWinners.push(this.doDraw(this.currentAward.allowDuplicate));
+        this.currentWinner = awardWinners.toString();
       }
       this.winners.push(awardWinners.toString());
       this.finishedAwards.push(this.currentAward);
       this.switchAward();
     }
-
     this.isDrawInProgress = !this.isDrawInProgress;
-    if (this.isDrawInProgress) {
-      this.drawButtonText = '停';
-    } else {
-      this.drawButtonText = '开始抽奖';
-    }
   }
 
   // draw one winner one time
   doDraw(allowDuplicate: boolean): string {
     let tmpWinner = this.participantList[this.getRandomInt()];
     // 如果奖项不允许重复，检查获奖人是否重复，重复了重新抽。
-    // TODO: fix this logic issue
     if (!allowDuplicate) {
       let isUserDuplicate = true;
       while (isUserDuplicate) {
         let tmpDuplicate = false;
         for (const item of this.winners) {
+          console.log(item.toString());
           if (item.toString().includes(tmpWinner)) {
             tmpDuplicate = true;
             tmpWinner = this.participantList[this.getRandomInt()];
             break;
           }
+        }
+        if (this.currentWinner.includes(tmpWinner)) {
+          tmpDuplicate = true;
+          tmpWinner = this.participantList[this.getRandomInt()];
         }
         if (!tmpDuplicate) {
           isUserDuplicate = tmpDuplicate;
@@ -113,5 +125,23 @@ export class LuckyDrawComponent implements OnInit {
     const Range = max - min;
     const Rand = Math.random();
     return(min + Math.round(Rand * Range));
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
